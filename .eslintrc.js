80b2a261f9f7ca88136d9252da63b19c5db03b26
kscript <1258@live.cn>
2022-07-19 07:10:09 +0000

    调整配置

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: ["error", 2, { "MemberExpression": 1 }],
+    semi: ["error", "never", { "beforeStatementContinuationChars": "always"}]
+  }
+}
